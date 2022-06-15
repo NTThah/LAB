afdcfd4ee465e2c7514b42cbfc3b07229cf5c07b
NguyenThanh <thnh.47@gmail.com>
2022-06-15 22:37:59 +0700

    LAB06

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Footer from "./FooterComponent";
 import Header from "./HeaderComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { Switch, Route, Redirect } from 'react-router-dom';
 class Main extends Component {
   constructor(props) {
@@ -55,6 +56,7 @@ class Main extends Component {
             dishes={this.state.dishes} />  } />
           <Route exact path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact}/>
+          <Route exact path='/aboutus' component={ () => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
